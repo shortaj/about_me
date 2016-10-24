2448a73468f416cec198cf64ae89c210a9aa1a68
Alex <ajshort2010@hotmail.com>
2016-10-23 23:04:31 -0700

    more subtle tuning of app.js and have begun testing. unresponsive but error free :/

--- app.js
+++ app.js
@@ -7,8 +7,12 @@ var questionArray = ['Do I speak more than one language?', 'Do I know how to fly
 var correctResponseArray = ['Correct! I can speak Japanese. As a high school exchange student and have traveled to Japan 5 times now.', 'Hooray! Correct! Do not trust me to fly a plane!', 'Good job, that is correct. It is a bunch of fun but reaaaaally dangerous.', 'Yep. It is beyond me at the moment and I have no current intentions of learning.', 'You can bet your bottom dollar that is correct.', 'That\'s correct, it is the meaning of life afterall.'];
 var incorrectResponseArray = ['Sorry, wrong. I speak Japanese. Not much use for it sadly besides my anime.', 'Nope, that is incorrect. I wish.', 'Bummer, that is wrong. I have done that alot!', 'Yep. It is beyond me at the moment and I have no current intentions of learning.', 'Wrong! I did say that I was into DnD. 1d8 18/20 x2', 'Well you had a couple tries, thanks for trying.'];
 var userAnswerArray = [];
-var yes = (userAnswerArray[i].toLowerCase() === 'y' || userAnswerArray[i].toLowerCase() === 'yes');
-var no = (userAnswerArray[i].toLowerCase() === 'no' || userAnswerArray[i].toLowerCase() === 'no');
+var yes = function() {
+  (userAnswerArray[i].toLowerCase() === 'y' || userAnswerArray[i].toLowerCase() === 'yes');
+};
+var no = function() {
+  (userAnswerArray[i].toLowerCase() === 'no' || userAnswerArray[i].toLowerCase() === 'no');
+};
 var correct = function() {
   console.log(correctResponseArray[i]);
   score++;
@@ -25,47 +29,44 @@ var error = function() {
   alert('Yes or no answers please.');
 };
 var questions = function() {
-  if (i > 5) {
+  while (i < 5) {
     userAnswerArray[i] = prompt(questionArray[i]);
-    while (i < 9) {
-      if (parseInt(userAnswerArray[i]) === 42 ) {
-        alert('Correct!!');
-        console.log('You got the right answer!');
-        break;
-      } else if (parseInt(userAnswerArray[i]) > 42) {
-        alert('Too high.');
-        i++;
-      } else {
-        alert('Too low.');
-        i++;
-      }
+    if (i === 0 && yes) {
+      correct;
+    } else if (i === 0 && no) {
+      incorrect;
+    } else if (i === 1 && yes) {
+      incorrect;
+    } else if (i === 1 && no) {
+      correct;
+    } else if (i === 2 && yes) {
+      correct;
+    } else if (i === 2 && no) {
+      incorrect;
+    } else if (i === 3 && yes) {
+      incorrect;
+    } else if (i === 3 && no) {
+      correct;
+    } else if (i === 4 && yes) {
+      correct;
+    } else if (i === 4 && no) {
+      incorrect;
+    } else {
+      error;
     }
-  } else {
-    while (i < 0) {
-      userAnswerArray[i] = prompt(questionArray[i]);
-      if (i === 0 && yes) {
-        correct;
-      } else if (i === 0 && no) {
-        incorrect;
-      } else if (i === 1 && yes) {
-        incorrect;
-      } else if (i === 1 && no) {
-        correct;
-      } else if (i === 2 && yes) {
-        correct;
-      } else if (i === 2 && no) {
-        incorrect;
-      } else if (i === 3 && yes) {
-        incorrect;
-      } else if (i === 3 && no) {
-        correct;
-      } else if (i === 4 && yes) {
-        correct;
-      } else if (i === 4 && no) {
-        incorrect;
-      } else {
-        invalids;
-      }
+  }
+  while (i < 9) {
+    userAnswerArray[i] = prompt(questionArray[i]);
+    if (parseInt(userAnswerArray[i]) === 42 ) {
+      alert('Correct!!');
+      console.log('You got the right answer!');
+      break;
+    } else if (parseInt(userAnswerArray[i]) > 42) {
+      alert('Too high.');
+      i++;
+    } else {
+      alert('Too low.');
+      i++;
     }
   }
 };
@@ -95,3 +96,6 @@ var tallyUp = function() {
     console.log('Thanks for trying!');
   }
 };
+questions;
+finish;
+tallyUp;
