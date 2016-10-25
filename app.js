11f892b6875130a860b07e31f70453bde15e5d23
Alex <ajshort2010@hotmail.com>
2016-10-24 20:05:45 -0700

    Finished the project and tested it. Very basic but so am I.

--- app.js
+++ app.js
@@ -3,99 +3,108 @@
 var score = 0;
 var invalids = 0;
 var i = 0;
-var questionArray = ['Do I speak more than one language?', 'Do I know how to fly a biplane?', 'Do I know how to herd cattle on a motorbike?', 'Do I know how to ride a unicycle?', 'Do I know critical strike range of a longsword and its multiplier off the top of my head?','What is my favorite number? You will have 4 tries at this.'];
-var correctResponseArray = ['Correct! I can speak Japanese. As a high school exchange student and have traveled to Japan 5 times now.', 'Hooray! Correct! Do not trust me to fly a plane!', 'Good job, that is correct. It is a bunch of fun but reaaaaally dangerous.', 'Yep. It is beyond me at the moment and I have no current intentions of learning.', 'You can bet your bottom dollar that is correct.', 'That\'s correct, it is the meaning of life afterall.'];
-var incorrectResponseArray = ['Sorry, wrong. I speak Japanese. Not much use for it sadly besides my anime.', 'Nope, that is incorrect. I wish.', 'Bummer, that is wrong. I have done that alot!', 'Yep. It is beyond me at the moment and I have no current intentions of learning.', 'Wrong! I did say that I was into DnD. 1d8 18/20 x2', 'Well you had a couple tries, thanks for trying.'];
+var questionArray = ['Do I speak more than one language?', 'Do I know how to fly a biplane?', 'Do I know how to herd cattle on a motorbike?', 'Do I know how to ride a unicycle?', 'Do I know critical strike range of a longsword, its multiplier, and damage off the top of my head?','What is my favorite number? You will have 4 tries at this.'];
+var correctResponseArray = ['Correct! I can speak Japanese. As a high school exchange student and have traveled to Japan 5 times now.', 'Hooray! Correct! Do not trust me to fly a plane!', 'Good job, that is correct. It is a bunch of fun but reaaaaally dangerous.', 'Yep. It is beyond me at the moment and I have no current intentions of learning.', 'You can bet your bottom dollar that is correct.'];
+var incorrectResponseArray = ['Sorry, wrong. I speak Japanese. Not much use for it sadly besides my anime.', 'Nope, that is incorrect. I wish.', 'Bummer, that is wrong. I have done that alot!', 'Yep. It is beyond me at the moment and I have no current intentions of learning.', 'Wrong! I did say that I was into DnD. 1d8 18/20 x2'];
+var userAnswer;
 var userAnswerArray = [];
-var yes = function() {
-  (userAnswerArray[i].toLowerCase() === 'y' || userAnswerArray[i].toLowerCase() === 'yes');
-};
-var no = function() {
-  (userAnswerArray[i].toLowerCase() === 'no' || userAnswerArray[i].toLowerCase() === 'no');
-};
-var correct = function() {
+var correct = function(a) {
+  userAnswerArray[i] = a;
   console.log(correctResponseArray[i]);
   score++;
   i++;
-  alert('CORRECT!');
+  return alert('CORRECT!');
 };
-var incorrect = function() {
+var incorrect = function(a) {
+  userAnswerArray[i] = a;
   console.log(incorrectResponseArray[i]);
   i++;
-  alert('WRONG!');
+  return alert('WRONG!');
 };
-var error = function() {
+var error = function(a) {
+  userAnswerArray[i] = a;
   invalids++;
-  alert('Yes or no answers please.');
+  return alert('Yes or no answers please.');
 };
-var questions = function() {
-  while (i < 5) {
-    userAnswerArray[i] = prompt(questionArray[i]);
+//var questions
+while (i < 5) {
+  userAnswer = prompt(questionArray[i]);
+  if (userAnswer === null) {
+    alert('Please play the game.');
+  } else {
+    var yes = (userAnswer.toLowerCase() === 'y' || userAnswer.toLowerCase() === 'yes');
+    var no = (userAnswer.toLowerCase() === 'no' || userAnswer.toLowerCase() === 'no');
     if (i === 0 && yes) {
-      correct;
+      correct(userAnswer);
     } else if (i === 0 && no) {
-      incorrect;
+      incorrect(userAnswer);
     } else if (i === 1 && yes) {
-      incorrect;
+      incorrect(userAnswer);
     } else if (i === 1 && no) {
-      correct;
+      correct(userAnswer);
     } else if (i === 2 && yes) {
-      correct;
+      correct(userAnswer);
     } else if (i === 2 && no) {
-      incorrect;
+      incorrect(userAnswer);
     } else if (i === 3 && yes) {
-      incorrect;
+      incorrect(userAnswer);
     } else if (i === 3 && no) {
-      correct;
+      correct(userAnswer);
     } else if (i === 4 && yes) {
-      correct;
+      correct(userAnswer);
     } else if (i === 4 && no) {
-      incorrect;
+      incorrect(userAnswer);
     } else {
-      error;
+      error(userAnswer);
     }
   }
-  while (i < 9) {
-    userAnswerArray[i] = prompt(questionArray[i]);
-    if (parseInt(userAnswerArray[i]) === 42 ) {
-      alert('Correct!!');
-      console.log('You got the right answer!');
-      break;
-    } else if (parseInt(userAnswerArray[i]) > 42) {
-      alert('Too high.');
-      i++;
-    } else {
-      alert('Too low.');
-      i++;
-    }
-  }
-};
-var finish = function() {
-  if (invalids === 1) {
-    console.log('Just one mistake. I get it. It\'s cool have a point on me.');
+}
+while (i < 9) {
+  userAnswerArray[i] = prompt(questionArray[5]);
+  if (parseInt(userAnswerArray[i]) === 42 ) {
+    alert('Correct!!');
+    console.log('You got the right answer!');
     score++;
-  } else if (invalids === 2) {
-    console.log('Couple mistakes no biggie!');
-  } else if (invalids === 3) {
-    console.log('Really? At this point I am suprised you did not just blerg the flerg.');
-  } else if (invalids === 4) {
-    console.log('Either you can\'t read, your curious, or dislike playing by the rules.');
-  } else if (invalids >= 5) {
+    break;
+  } else if (parseInt(userAnswerArray[i]) > 42) {
+    alert('Too high. This is the ' + (i-4)+ '/4 attempts.');
+    i++;
+  } else if (parseInt(userAnswerArray[i]) < 42){
+    alert('Too low. This is the ' + (i-4)+ '/4 attempts.');
+    i++;
+  } else if (userAnswerArray[i]=== null) {
+    alert('Please play the game.');
+  } else {
+    alert('Numbers please.')
+    i++;
+  }
+}
+
+var finish = function(a) {
+  if (a === 1) {
+    score++;
+    return alert('Just one mistake. I get it. It\'s cool have a point on me.');
+  } else if (a === 2) {
+    return alert('Couple mistakes no biggie!');
+  } else if (a === 3) {
+    return alert('Really? At this point I am suprised you did not just blerg the flerg.');
+  } else if (a === 4) {
+    return alert('Either you can\'t read, your curious, or dislike playing by the rules.');
+  } else if (a >= 5) {
     for (var k = 0; k <= 5; k++) {
       console.log(userAnswerArray[k]);
     }
-    console.log('Welp are you happy with your work? Smart ass.');
+    return alert('Welp are you happy with your work?');
   }
 };
-var tallyUp = function() {
-  if (score === 5) {
-    console.log('Congratz you got 100%!! It\'s like you already know me.');
-  } else if (score >= 3) {
-    console.log(score + '/5 isn\'t too bad! Thanks for playing.');
+var tallyUp = function(a) {
+  if (a >= 5) {
+    return alert('Congratz you did great! It\'s like you already know me.');
+  } else if (a >= 3) {
+    return alert(score + '/6 isn\'t too bad! Thanks for playing.');
   } else {
-    console.log('Thanks for trying!');
+    return alert('Thanks for trying!');
   }
 };
-questions;
-finish;
-tallyUp;
+finish(invalids);
+tallyUp(score);
